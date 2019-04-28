d92061b78f6aef3d84399d1a287fd3a9d715fbf7
MasahiroTakeda <taketake.12326@gmail.com>
2019-04-28 18:42:25 +0900

    javascript

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,6 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 
     return render(request, 'blog/post_list.html', {'posts':posts})
+def deposit(request):
+    return render(request, 'blog/deposit_list.html')
 # Create your views here.
