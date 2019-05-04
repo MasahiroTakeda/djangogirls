88833fe499ebfb6155a950222d6c252b0272c3be
MasahiroTakeda <taketake.12326@gmail.com>
2019-05-04 16:49:01 +0900

    よっしゃ

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,8 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 
     return render(request, 'blog/post_list.html', {'posts':posts})
+
 def deposit(request):
     return render(request, 'blog/deposit_list.html')
-# Create your views here.
+
+
